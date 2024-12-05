cf9e236e1810d408dd0c1fa05d605ea72d477b82
Emma <im.emm@live.com>
2024-12-05 16:06:33 +0100

    Not Found page

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,7 +1,22 @@
-import Layout from "../../Components/Layout";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <Layout>NotFound</Layout>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-h-[400px] max-w-[400px] flex flex-col items-center">
+        <img
+          className="h-1/2 object-cover"
+          src="../../../public/img/404.png"
+          alt="404 Not found"
+        />
+        <Link to={"/all"}>
+          <a className="hover:decoration-blue-500 hover:underline hover:underline-offset-4">
+            Back to home
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
